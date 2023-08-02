1c6c21084f91166df49ece037e08ec55efc108dc
takaha <torikabuto1018@gmail.com>
2023-08-02 14:55:55 +0900

    競合解決

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import AscAboutInteresetComponent from './AscAboutInterestComponent.jsx';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
