9f131b0860934b4afb6c7de6f71ef16b521baac5
yakitate Ja pan <flx503@outlook.jp>
2023-08-02 14:32:55 +0900

    calendar event button作成

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid'; // 追加
 import jaLocale from '@fullcalendar/core/locales/ja';
+
 const thisMonth = () => {
     const today = new Date();
     return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
@@ -11,31 +12,42 @@ const thisMonth = () => {
     )}`;
   };
 const Calendar = () => {
+  const [events, setEvents] = useState([
+    // 初期のイベントデータ（必要に応じてカスタマイズしてください）
+    { title: 'イベント1', start: '2023-08-02T10:00:00', end: '2023-08-02T12:00:00' },
+    { title: 'イベント2', start: '2023-08-03T14:00:00', end: '2023-08-03T16:00:00' },
+  ]);
+
+  const handleAddEvent = () => {
+    const title = prompt('イベント名を入力してください:');
+    if (title) {
+      const newEvent = {
+        title,
+        start: new Date(),
+        end: new Date(),
+      };
+
+      setEvents([...events, newEvent]);
+    }
+  };
+
   return (
-    <FullCalendar
-        plugins={[ dayGridPlugin ,timeGridPlugin]}
-        initialView="timeGridDay"
-        locales={[jaLocale]}
-        locale='ja'
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridDay',
-        }}
-        events={[
-          {
-            id: "c",
-            start: "2023-08-02T12:00:00",
-            end: "2023-08-02T13:00:00",
-            title: "針供np養",
-            description: "古くなった針などを神社に納めて供養する",
-            backgroundColor: "blue",
-            borderColor: "blue",
-            editable: true
-          },            
-          { title: "event 2", date: `${thisMonth()}-01`},
-          ]}
-    />
+    <>
+      <button onClick={handleAddEvent}>イベントを追加</button>
+
+      <FullCalendar
+          plugins={[ dayGridPlugin ,timeGridPlugin]}
+          initialView="timeGridDay"
+          locales={[jaLocale]}
+          locale='ja'
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridDay',
+          }}
+          events={events}
+      />
+     </>
   )
 }
 
