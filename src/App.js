a385c1f60637394ef0821b0271f4b6f7bdf3ede7
kenketsu <razgriz.memai02723@gmail.com>
2023-08-02 11:35:41 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import { LoadScript } from "@react-google-maps/api";
 import './App.css';
-import GoogleMap from './components/map';
 
 function App() {
   return (
     <div className="App">
-      <LoadScript googleMapsApiKey={process.env.REACT_APP_MAP_API_KEY} libraries={["places"]}>
-        <GoogleMap />
-      </LoadScript>
+
     </div>
   );
 }
