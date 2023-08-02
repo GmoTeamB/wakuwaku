976905730993cdaa8e5a287ccb0e6e8998572c1a
yakitate Ja pan <flx503@outlook.jp>
2023-08-02 15:12:22 +0900

    calendar実装

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -4,6 +4,74 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid'; // 追加
 import jaLocale from '@fullcalendar/core/locales/ja';
 
+const json = 
+{
+    "@odata.context":"https://graph.microsoft.com/v1.0/$metadata#Collection(microsoft.graph.scheduleInformation)",
+    "value":[
+        {
+            "scheduleId":"AlexW@contoso.OnMicrosoft.com",
+            "availabilityView":"111111002222222200000000000000000000",
+            "scheduleItems":[
+                {
+                    "status":"Tentative",
+                    "start":{
+                        "dateTime":"2018-08-06T09:00:00.0000000",
+                        "timeZone":"Pacific Standard Time"
+                    },
+                    "end":{
+                        "dateTime":"2018-08-06T10:30:00.0000000",
+                        "timeZone":"Pacific Standard Time"
+                    }
+                },
+                {
+                    "status":"Busy",
+                    "start":{
+                        "dateTime":"2023-08-02T11:00:00.0000000",
+                        "timeZone":"Pacific Standard Time"
+                    },
+                    "end":{
+                        "dateTime":"2023-08-02T13:00:00.0000000",
+                        "timeZone":"Pacific Standard Time"
+                    }
+                }
+            ],
+            "workingHours":{
+                "daysOfWeek":[
+                    "monday",
+                    "tuesday",
+                    "wednesday",
+                    "thursday",
+                    "friday"
+                ],
+                "startTime":"08:00:00.0000000",
+                "endTime":"17:00:00.0000000",
+                "timeZone":{
+                    "@odata.type":"#microsoft.graph.customTimeZone",
+                    "bias":480,
+                    "name":"Customized Time Zone",
+                    "standardOffset":{
+                        "time":"02:00:00.0000000",
+                        "dayOccurrence":1,
+                        "dayOfWeek":"sunday",
+                        "month":11,
+                        "year":0
+                    },
+                    "daylightOffset":{
+                        "daylightBias":-60,
+                        "time":"02:00:00.0000000",
+                        "dayOccurrence":2,
+                        "dayOfWeek":"sunday",
+                        "month":3,
+                        "year":0
+                    }
+                }
+            }
+        }
+    ]
+}
+
+
+
 const thisMonth = () => {
     const today = new Date();
     return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
@@ -19,17 +87,37 @@ const Calendar = () => {
   ]);
 
   const handleAddEvent = () => {
-    const title = prompt('イベント名を入力してください:');
+    let title = "test"
+    let startDateTime  = ""
+    let endDateTime  = ""
+
+z
+    for (const scheduleItem of json.value[0].scheduleItems) {
+      if (scheduleItem.status === "Busy") {
+        
+          startDateTime = scheduleItem.start.dateTime;
+          console.log("Start Date and Time:", startDateTime);
+          break; // 最初の Busy スケジュールアイテムを見つけたらループを終了
+      }
+    }
+    for (const scheduleItem of json.value[0].scheduleItems) {
+      if (scheduleItem.status === "Busy") {
+          startDateTime = scheduleItem.start.dateTime;
+          endDateTime = scheduleItem.end.dateTime;
+          console.log("Start Date and Time:", startDateTime);
+          break; // 最初の Busy スケジュールアイテムを見つけたらループを終了
+      }
+    }
     if (title) {
       const newEvent = {
         title,
-        start: new Date(),
-        end: new Date(),
+        start: startDateTime,
+        end: endDateTime,
       };
-
       setEvents([...events, newEvent]);
     }
   };
+  
 
   return (
     <>
