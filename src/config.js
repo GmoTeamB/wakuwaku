cd719331f8f11544519ee203242fb7de7814ae26
Tatsuya Tanaka <kanatatsu64@gmail.com>
2023-08-02 15:47:07 +0900

    add calendar api client

--- src/config.js
+++ src/config.js
@@ -64,5 +64,6 @@ export const scopes = ["User.Read", "Mail.Read", "Calendars.Read"];
 // Add here the endpoints for MS Graph API services you would like to use.
 export const graphConfig = {
     graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
-    graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages"
+    graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages",
+    graphCalendarEndpoint: "https://graph.microsoft.com/v1.0/me/calendar/getschedule",
 };
