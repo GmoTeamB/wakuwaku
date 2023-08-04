105f0c98b2968fcd5af6007328ed41bc1a71e728
takaha <torikabuto1018@gmail.com>
2023-08-04 12:17:08 +0900

    リダイレクト時にインスタンスを生成するように変更

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,9 @@ import Auth from './components/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth/>
+  <React.StrictMode>
+    <Auth/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
